f4cea690be6298c51948a6720a111bd85b2e5f64
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-12-01 15:06:23 +0000

    Fill Out Mongoose Schemas

--- models/comment.js
+++ models/comment.js
@@ -3,5 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
-
+    comment: { type: String, required: true},
+    author: { type: Schema.Types.ObjectId, ref: "User"},
+    childComments: [],
 })
+
+module.exports = mongoose.model('Comment', CommentSchema);
---
+++ models/post.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    title: {type: String, required: true},
+    text: {type: String, required: true},
+    comments: [],
+    date: {type: Date, required: true},
+    likes: {type: Number, required: true, min: 0, max: 999},
+
+})
+
+module.exports = mongoose.model('Post', PostSchema);
