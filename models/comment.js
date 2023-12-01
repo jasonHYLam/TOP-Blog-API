642ce7eefae4618712a05bf1b4b10c3f5648ba38
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-12-01 15:07:21 +0000

    Add TimeStamp To Comment Schema

--- models/comment.js
+++ models/comment.js
@@ -6,6 +6,7 @@ const CommentSchema = new Schema({
     comment: { type: String, required: true},
     author: { type: Schema.Types.ObjectId, ref: "User"},
     childComments: [],
+    timeStamp: { type: Date, required: true}
 })
 
 module.exports = mongoose.model('Comment', CommentSchema);
