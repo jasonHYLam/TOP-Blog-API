06dc7dbca162dc5a5a124892c7b52b1b3f214c87
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-12-04 15:18:42 +0000

    Remove Comments Array From Post Schema

--- models/post.js
+++ models/post.js
@@ -5,7 +5,6 @@ const Schema = mongoose.Schema;
 const PostSchema = new Schema({
     title: {type: String, required: true},
     text: {type: String, required: true},
-    comments: [],
     date: {type: Date, required: true},
     likes: {type: Number, required: true, min: 0, max: 999},
     published_status: Boolean,
