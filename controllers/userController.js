6b50dd8b2a05311972a39c9471269d8d6c0b5617
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-12-11 21:29:51 +0000

    Push To Repo

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,8 +5,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const asyncHandler = require('express-async-handler');
 const { body } = require('express-validator');
-
-
+const bcrypt = require('bcryptjs');
 
 // exports.home_get = asyncHandler(async (req, res, next) => {
 //     res.json({
@@ -29,14 +28,18 @@ exports.signup_post = [
     .trim()
     .escape(),
 
-
     asyncHandler(async (req, res, next) => {
         console.log('I assume this is gonna work...')
         console.log(req.body)
         console.log(req.body.username)
         console.log(req.body.password)
 
-        await new User({})
+        const user = new User({
+            username: req.body.username,
+            password: req.body.password,
+        })
+
+
 
     })
 
