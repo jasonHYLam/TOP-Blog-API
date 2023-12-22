b865e8727f95251f093e5ff339bb5ded710000f1
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-12-22 10:48:12 +0000

    Create Admin_Create_Post Route For Admin App

--- routes/index.js
+++ routes/index.js
@@ -57,9 +57,9 @@ router.put('/home/:postid/:commentid', commentController.comment_update);
 router.delete('/home/:postid/:commentid', commentController.comment_delete);
 
 // for admin app
-// router.post('/login_admin', adminController.admin_login, requireAuth)
 router.post('/login_admin', adminController.admin_login)
 router.get('/admin_all_posts', requireAdminAuth, adminController.all_posts);
+router.get('/admin_create_posts', requireAdminAuth, adminController.create_post);
 
 
 
