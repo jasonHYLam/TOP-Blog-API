b6d0cacf4c68e4d6a58cfb1405da5528c1ef7215
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-12-29 17:56:53 +0000

    Add Formatted Date Property To Post Model

--- models/post.js
+++ models/post.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 const Schema = mongoose.Schema;
 
@@ -9,6 +10,11 @@ const PostSchema = new Schema({
     author: { type: Schema.Types.ObjectId, ref: "User"},
     likes: {type: Number, required: true, min: 0, max: 999, default: 0},
     published_status: {type: Boolean, default: false},
+})
+
+// better time display
+PostSchema.virtual('timeStampFormatted').get(function() {
+    return DateTime.fromJSDate(this.timeStamp).toFormat('T dd/LL/yy')
 })
 
 module.exports = mongoose.model('Post', PostSchema);
