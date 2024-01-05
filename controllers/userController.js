429c9eeedb45c64819c51051ab0cc20a76756370
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-05 12:47:47 +0000

    Change Cookie Property

--- controllers/userController.js
+++ controllers/userController.js
@@ -55,8 +55,8 @@ exports.login_post = asyncHandler(async (req, res, next) => {
         // res.cookie('token', token, )
         res.cookie('token', token, {
             httpOnly: true,
-            // secure: true,
-            // sameSite: 'none'
+            secure: true,
+            sameSite: 'None'
         })
         .json(
             {
